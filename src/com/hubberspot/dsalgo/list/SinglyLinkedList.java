5512d94edd38d03d81cb235211e6fc3688e323fd
Ashishyadav07 <71344451+Ashishyadav07@users.noreply.github.com>
2022-08-20 21:45:39 +0530

    Remove Dupilcate In Sorted List
    I hope u accept my changes as it will help student to get the code of all the programs which are in the video

--- src/com/hubberspot/dsalgo/list/SinglyLinkedList.java
+++ src/com/hubberspot/dsalgo/list/SinglyLinkedList.java
@@ -205,6 +205,21 @@ public class SinglyLinkedList {
 		}
 		return mainPtr;
 	}
+	
+	 public void removeDuplicatesFromSortedList() {
+            	 if(head == null) {
+            		 return;
+            	 }
+            	 Node current = head;
+            	 while(current != null && current.next != null) {
+            		 if(current.data == current.next.data) {
+            			 current.next = current.next.next;
+            		 }
+            		 else {
+            			 current = current.next;
+            		 }
+            	 }
+             }
 
 	public ListNode insertInSortedList(int value) {
 		ListNode newNode = new ListNode(value);
